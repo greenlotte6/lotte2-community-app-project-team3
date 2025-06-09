43697f28c0003b42a3a022b0f27daf338d8d1f02
beermoon <Feathermoon0318@gmail.com>
2025-06-09 17:10:41 +0900

    add landing react

--- workie-talkie/src/components/landing/FAQSection.jsx
+++ workie-talkie/src/components/landing/FAQSection.jsx
@@ -4,19 +4,19 @@ import React from "react";
 const FAQSection = () => {
   return (
     <section className="faq-section">
-      <div className="faq-section__container">
-        <div className="faq-section__content">
-          <div className="faq-section__text">
+      <div className="faq-container">
+        <div className="faq-content">
+          <div className="faq-text">
             <h1>비즈니스 고객 제보</h1>
             <p>
               워키토키를 이용하는 중 불편함을 느끼셨나요? <br />
               지금 바로 알려주세요
             </p>
-            <a href="/main/faq.html" className="faq-section__button">
+            <a href="/main/faq.html" className="faq-button">
               고객센터
             </a>
           </div>
-          <div className="faq-section__image">
+          <div className="faq-image">
             <img src="/images/main/faq-icon.png" alt="고객 지원" />
           </div>
         </div>
@@ -25,4 +25,4 @@ const FAQSection = () => {
   );
 };
 
-export default FAQSection;
+export { FAQSection };
--- workie-talkie/src/components/landing/Header.jsx
+++ workie-talkie/src/components/landing/Header.jsx
@@ -8,12 +8,12 @@ export const Header = () => {
       <div className="header-wrapper" id="landing">
         <header className="header">
             <div className="header-left">
-              <Link to="/main/pricing">가격</Link>
-              <Link to="/main/faq">도움 센터</Link>
+              <Link to="/pricing">가격</Link>
+              <Link to="/faq">도움 센터</Link>
             </div>
 
             <div className="header-center">
-              <Link to="/index" className="logo">
+              <Link to="/" className="logo">
                 <img src="/images/logo_title.png" alt="Logo" />
               </Link>
             </div>
