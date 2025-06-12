46510a8a7411d6670ec7c2f3fe6815c734b22845
minsung <dpsdy1@gmail.com>
2025-06-12 12:48:22 +0900

    ADD

---
+++ Workie/src/main/java/kr/co/workie/service/DriveService.java
@@ -0,0 +1,48 @@
+package kr.co.workie.service;
+
+import kr.co.workie.dto.DriveItemDTO;
+import kr.co.workie.entity.DriveItem;
+import kr.co.workie.entity.User;
+import kr.co.workie.repository.DriveItemRepository;
+import kr.co.workie.repository.UploadFilesRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DriveService {
+
+    private final DriveItemRepository driveItemRepository;
+    private final UploadFilesRepository uploadFilesRepository;
+
+
+    public List<DriveItemDTO> listDriveItems(User user, Long parentId) {
+        return driveItemRepository.findByUserAndParentId(user, parentId).stream()
+                .map(item -> new DriveItemDTO(
+                        item.getDno(),
+                        item.getName(),
+                        item.getType().name(),
+                        item.getParentId(),
+                        item.getSize(),
+                        item.getCreatedAt()
+                )).toList();
+    }
+
+    public DriveItem createFolder(User user, String name, Long parentId) {
+        return driveItemRepository.save(DriveItem.builder()
+                .name(name)
+                .type(DriveItem.fileType.FOLDER)
+                .user(user)
+                .parentId(parentId)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build());
+    }
+
+    public void deleteDriveItem(Long dno) {
+        driveItemRepository.deleteById(dno);
+    }
+}
