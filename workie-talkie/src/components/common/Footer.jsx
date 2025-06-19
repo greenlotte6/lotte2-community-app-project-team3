767a62e52e93021fdf34c67b8064f5aa22efb2c7
minsung <dpsdy1@gmail.com>
2025-06-19 16:58:59 +0900

    add

--- workie-talkie/src/components/common/Footer.jsx
+++ workie-talkie/src/components/common/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <>
-      <div className="footer">0.0.9-SNAPSHOT</div>
+      <div className="footer">0.1.0-SNAPSHOT</div>
     </>
   );
 };
