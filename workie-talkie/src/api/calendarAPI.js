840b356e1783ef4aed58b66af448fc98899d6b85
tkdenddl1324 <tkdenddl1324@gmail.com>
2025-06-26 14:47:41 +0900

    토큰테스트

--- workie-talkie/src/api/calendarAPI.js
+++ workie-talkie/src/api/calendarAPI.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "./axios";
 import { CALENDAR_ADD, CALENDAR } from "./http";
 
 //캘린더 관련
--- workie-talkie/src/api/projectAPI.js
+++ workie-talkie/src/api/projectAPI.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "./axios";
 import { PROJECT, PROJECT_ADD } from "./http";
 
 export const postProject = async (data) => {
