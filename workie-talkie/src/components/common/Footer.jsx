36f50f1792f89b39da1c3733e8e1e79a0ed5612f
tkdenddl1324 <tkdenddl1324@gmail.com>
2025-06-27 10:08:37 +0900

    1.1.0

--- workie-talkie/src/components/common/Footer.jsx
+++ workie-talkie/src/components/common/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <>
-      <div className="footer">0.1.5-SNAPSHOT</div>
+      <div className="footer">1.0.0-SNAPSHOT</div>
     </>
   );
 };
